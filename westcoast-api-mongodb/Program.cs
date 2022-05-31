b53b6a9070844110d68912b11927c5f33a033bf9
MichaelGustavsson <michael.gustavsson@skaftoskorpan.se>
2022-05-31 15:47:49 +0200

    Finalized MongoDB

--- westcoast-api-mongodb/Program.cs
+++ westcoast-api-mongodb/Program.cs
@@ -1,6 +1,13 @@
+using westcoast_api_mongodb.Models;
+using westcoast_api_mongodb.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.Configure<MongoDbSettings>(builder.Configuration.GetSection("MongoDB"));
+
+// Dependency injection...
+builder.Services.AddSingleton<MongoDbService>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -12,8 +19,8 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+  app.UseSwagger();
+  app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
--- westcoast-api-mongodb/Services/MongoDbService.cs
+++ westcoast-api-mongodb/Services/MongoDbService.cs
@@ -1,8 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.Extensions.Options;
+using MongoDB.Bson;
 using MongoDB.Driver;
 using westcoast_api_mongodb.Models;
 namespace westcoast_api_mongodb.Services
@@ -17,6 +14,16 @@ namespace westcoast_api_mongodb.Services
       IMongoDatabase database = client.GetDatabase(settings.Value.DatabaseName);
 
       _vehicleListCollection = database.GetCollection<Vehicle>(settings.Value.CollectionName);
+    }
+
+    public async Task<List<Vehicle>> GetVehiclesAsync()
+    {
+      return await _vehicleListCollection.Find(new BsonDocument()).ToListAsync();
+    }
+
+    public async Task CreateVehicleAsync(Vehicle vehicle)
+    {
+      await _vehicleListCollection.InsertOneAsync(vehicle);
     }
   }
 }
